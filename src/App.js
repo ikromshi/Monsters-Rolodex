da683379358a2ff751b04bd9b6a4e8a4c9a04b69
Ikrom Numonov <numanovikrom454@gmail.com>
2022-03-17 22:03:23 -0400

    converted class components into function components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,57 @@
-import { Component } from "react";
+import { useState, useEffect } from "react"; // importing the useState hook to replace this.state (class components)
 import SearchBox from "./components/search-box/search-box.components";
 import CardList from "./components/card-list/card-list.component";
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ""
-    }  
-  }
 
-  async componentDidMount() {
-    const promise = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!promise.ok) {throw Error(promise.statusText)}
-    const data = await promise.json(); 
+const App = () => {
+  /** 
+   * useState spits out an array with two elements => [value, setValue] => setValue() is a function
+   * create anotber useState when there's a need for more variables to store
+   */
+  const [searchField, setSearchField] = useState(""); 
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonstersArr, setFilteredMonsters] = useState(monsters);
 
-    this.setState(() => {
-      return {monsters: data}
-    }
-  )}
+  /**
+   * useEffect uses the side effects of JS to prevent infinite rendering (eqv componentDidMount)
+   * it has a callback function to run the code we give it, and an array of dependencies
+   * it only runs when the page loads and when the dependency array changes
+   */
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => setMonsters(users));
+  }, [])
 
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLowerCase();
-
-    this.setState(() => {
-      return {searchField};
-    })
-  }
-
-  render() {
-    const { monsters, searchField} = this.state;
-    const { onSearchChange } = this;
-
+  useEffect(() => {
     const filteredMonsters = monsters.filter(monster => {
       return monster.name.toLowerCase().includes(searchField);
     })
+    setFilteredMonsters(filteredMonsters);
+  }, [monsters, searchField]) // filter whenever monsters[] or searchField"" changes
 
-    return (
-      <div className="App">
-        <h1 className="app-title"> Monsters Rolodex</h1>
-        <SearchBox onChangeHandler={onSearchChange} placeholder="search monsters" className="monsters-search-box"/>
-        <CardList monsters={filteredMonsters}/>
-
-      </div>
-    );
+  /**
+   * Sets the value of searchField to what the user inputs, by calling setSearchField()
+   */
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLowerCase();
+      setSearchField(searchFieldString);
   }
+
+  return (
+    <div className="App">
+      <h1 className="app-title"> Monsters Rolodex</h1>
+      <SearchBox 
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange} 
+        placeholder="search monsters" 
+      />
+
+      <CardList monsters={filteredMonstersArr}/>
+
+    </div>
+  );
 }
-  
 
 export default App;
--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -1,17 +1,14 @@
-import { Component } from "react";
 import "./card.style.css";
 
-class Card extends Component {
-    render() {
-        const { id, name, email } = this.props.monster;
-        return (
-            <div key={id} className="card-container">
-                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}></img>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        )
-    }
+const Card = ({ monster }) => {
+    const { id, name, email } = monster;
+    return (
+        <div key={id} className="card-container">
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}></img>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
 }
 
 export { Card as default};
