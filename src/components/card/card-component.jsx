d318225d72f566a8bf0b756eccab1b75beaec501
Ikrom Numonov <numanovikrom454@gmail.com>
2022-07-25 17:39:10 -0400

    changed the card.styles.css into card.styles.jsx using styled components

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -1,13 +1,13 @@
-import "./card.style.css";
+import { CardContainer } from "./card.style";
 
 const Card = ({ monster }) => {
     const { id, name, email } = monster;
     return (
-        <div key={id} className="card-container">
+        <CardContainer key={id}>
             <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}></img>
             <h2>{name}</h2>
             <p>{email}</p>
-        </div>
+        </CardContainer>
     )
 }
 
